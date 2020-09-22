b0af4a8225e90c968315497421ddd1026616a210
Jaeeun98 <wodms4525@naver.com>
2020-09-23 00:29:18 +0900

    Title : Add
    JS color change add

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 const canvas  = document.querySelector('canvas');
-const color = document.querySelectorAll('.color');
+const colors = document.querySelectorAll('.color');
 const range = document.querySelector('.rangeControls');
 const saveBtn = document.querySelector('saveBtn');
 const fillBtn = document.querySelector('.fillBtn');
 const ctx = canvas.getContext('2d');
 
 // default
-const defaultColor = color[0].style.background;
+const defaultColor = colors[0].style.background;
 const canvasSize = 500;
 let painting = false;
 let filling = false;
@@ -18,8 +18,6 @@ ctx.fillStyle = '#fff';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.strokeStyle = defaultColor;
 
-
-
 function stopPainting(){
 	painting = false;
 }
@@ -27,6 +25,7 @@ function startPainting() {
 	painting = true;
 }
 
+//dwaring
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
@@ -39,9 +38,20 @@ function onMouseMove(event){
     }
 }
 
+//colorChange
+function colorChange(event){    
+    const color = event.target.style.background;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
+
+
 if(canvas){
     canvas.addEventListener('mousemove', onMouseMove);
     canvas.addEventListener('mousedown', startPainting);
     canvas.addEventListener('mouseup', stopPainting);
-
 }
+
+Array.from(colors).forEach(color =>
+    color.addEventListener("click", colorChange)
+);
