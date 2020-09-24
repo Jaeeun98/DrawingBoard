272a4bd37fc73c08d71e5f03a750feee2c24e247
kwon Jaeeun <58484758+Jaeeun98@users.noreply.github.com>
2020-09-24 10:47:09 +0900

    Title : Add
    JS mouse leave, stop Painting add

--- js/index.js
+++ js/index.js
@@ -70,6 +70,7 @@ function canvasFill(){
  canvas.addEventListener('mousemove', onMouseMove);
  canvas.addEventListener('mousedown', startPainting);
  canvas.addEventListener('mouseup', stopPainting);
+ canvas.addEventListener('mouseleave', stopPainting);
  canvas.addEventListener('click', canvasFill);
 
 
